c1cdf672ea20c673c292de47303b558ccf9d6a28
mehedi-hassan-sohan <mehedihassansohan80@gmail.com>
2023-06-12 02:23:24 +0600

    jwt payload updated

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ async function run() {
        const filter = { _id: new ObjectId(id) };
        const updateDoc = {
         $set: {
-          role: 'instructor',
+           role: 'instructor',
         },
       };
 
