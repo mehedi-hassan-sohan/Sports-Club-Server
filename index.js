0e48528dd92318d4bead97353241c86df7281b51
mehedi-hassan-sohan <mehedihassansohan80@gmail.com>
2023-06-12 02:21:53 +0600

    vertify admin from backend with token

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ async function run() {
      });
 
       app.patch('/students/instructor/:id', async (req, res) => {
-      const id = req.params.id;
+       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
